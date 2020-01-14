575299de6e4a0c5dfa296883305dbd7c31091a1d
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-01-14 10:09:01 -1000

    Initial Commit

---
+++ HackWhich1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackWhich1
+//
+//  Created by Reginald T King on 1/14/20.
+//  Copyright © 2020 Reginald T King. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
